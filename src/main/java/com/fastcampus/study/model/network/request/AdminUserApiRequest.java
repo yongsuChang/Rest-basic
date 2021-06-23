d6a53907294e85b1b0a7f18762f61ddaa1d6bb79
yongsuChang <77391211+yongsuChang@users.noreply.github.com>
2021-06-23 17:25:07 +0900

    [feat] Added CRUD API for AdminUser
    - Added CRUD API for AdminUser
    
    - AdminUser용 CRUD API 추가

---
+++ src/main/java/com/fastcampus/study/model/network/request/AdminUserApiRequest.java
@@ -0,0 +1,36 @@
+package com.fastcampus.study.model.network.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AdminUserApiRequest {
+
+    private Long id;
+
+    private String account;
+
+    private String password;
+
+    private String status;
+
+    private String role;
+
+    private LocalDateTime lastLoginAt;
+
+    private LocalDateTime passwordUpdatedAt;
+
+    private int loginFailCount;
+
+    private LocalDateTime registeredAt;
+
+    private LocalDateTime unregisteredAt;
+
+}
