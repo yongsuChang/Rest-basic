89242e94b25156750c34672010a5d5b8e698a52c
yongsuChang <77391211+yongsuChang@users.noreply.github.com>
2021-07-01 16:51:04 +0900

    [feat] Added front views
    - Added view pages
    
    - 뷰 페이지 추가

---
+++ src/main/java/com/fastcampus/study/model/network/response/UserOrderInfoApiResponse.java
@@ -0,0 +1,16 @@
+package com.fastcampus.study.model.network.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserOrderInfoApiResponse {
+
+    private UserApiResponse userApiResponse;
+
+}
